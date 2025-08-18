a3645ba7eb1c5f29375f237d65f5540ba4f4b1ab
mae-kelly <maeve.kelly444@gmail.com>
2025-08-17 20:42:39 -0400

    new

--- run_intermediate.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "📊 Starting CEX Arbitrage Bot"
-echo "Make sure your .env file has exchange API keys!"
-python3 src/orchestrator_m1_fixed.py
--- run_simple.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo "🔰 Starting Simple Arbitrage Monitor"
-python3 simple_bot.py
